bc7aff994614d1f2b5b398897827853feba0a0e6
KarthikThonni <ktho0051@student.monash.edu>
2026-05-21 22:20:25 +1000

    chart2_asx200yearly competed

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -1,3 +1,3 @@
 vegaEmbed("#global_index_performance", "vis/global_index_performance.json").catch(console.error);
 
-vegaEmbed("#sector_makeup_comparison", "vis/sector_makeup_comparison.json").catch(console.error);
+vegaEmbed("#asx_previous_years", "vis/asx_previous_years.json").catch(console.error);
