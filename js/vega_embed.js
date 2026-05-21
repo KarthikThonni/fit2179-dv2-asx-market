68a65d929fc2b25dcd250f8c64dfd86c48947fb8
KarthikThonni <ktho0051@student.monash.edu>
2026-05-22 00:14:55 +1000

    chart3_sector complete

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -1,3 +1,5 @@
 vegaEmbed("#global_index_performance", "vis/global_index_performance.json").catch(console.error);
 
 vegaEmbed("#asx_previous_years", "vis/asx_previous_years.json").catch(console.error);
+
+vegaEmbed("#sector_makeup", "vis/sector_makeup.json").catch(console.error);
