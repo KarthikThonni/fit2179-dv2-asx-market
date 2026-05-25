fa9f79ec253b5506106ce192939b8d4ec5f07d4e
KarthikThonni <ktho0051@student.monash.edu>
2026-05-25 16:54:23 +1000

    graphs done

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -6,10 +6,16 @@ vegaEmbed("#sector_makeup", "vis/sector_makeup.json").catch(console.error);
 
 vegaEmbed("#sector_growth", "vis/sector_growth.json").catch(console.error);
 
-vegaEmbed("#best_worst_companies", "vis/best_worst_companies.json").catch(console.error);
-
 vegaEmbed("#market_cap_vs_growth", "vis/market_cap_vs_growth.json").catch(console.error);
 
 vegaEmbed("#market_concentration", "vis/market_concentration.json").catch(console.error);
 
-vegaEmbed("#sector_size_vs_growth", "vis/sector_size_vs_growth.json").catch(console.error);
+vegaEmbed("#company_locations_map", "vis/company_location_map.json").catch(console.error);
+
+vegaEmbed("#company_treemap", "vis/company_treemap.json").catch(console.error);
+
+vegaEmbed("#sector_contribution", "vis/sector_contribution.json").catch(console.error);
+
+vegaEmbed("#asx_daily_area", "vis/asx_daily_area.json").catch(console.error);
+
+vegaEmbed("#sector_daily_lines", "vis/sector_daily_lines.json").catch(console.error);
